08cb171ff4b12485966b2f9d9fca58db4c10519d
Bravolakmedia <olakunlebravo@gmail.com>
2024-09-03 15:52:09 +0100

    Making adjustments in vercel.json and package.json files

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document, {
    customCssUrl:
-   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui 4.15.5/swagger-ui.min.css',
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
    customJs:[
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui 4.15.5/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui 4.15.5/swagger-ui-standalone-preset.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
    ],
   });
   await app.listen(3000);
